f0f5c7cf689ab30e01495c8bf34b3205b28c7e56
McRamsey18 <lethlokoramsey@gmail.com>
2021-08-18 09:52:40 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project_update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String HELLO_WORLD= Console.ReadLine();
+
+            Console.WriteLine("ID");
+            String ID= Console.ReadLine();
+
+            Console.WriteLine("SURNAME");
+            String SURNAME = Console.ReadLine();
+
+            Console.WriteLine("FIRST NAME");
+            String FIRSTname = Console.ReadLine();
+
+            Console.WriteLine("GENDER");
+            String Gender = Console.ReadLine();
+
+            Console.WriteLine("DATE OF BIRTH");
+            String DATEofBIRTH = Console.ReadLine();
+
+            Console.WriteLine("PROVINCE");
+            String PROVINCE = Console.ReadLine();
+        }
+    }
+}
